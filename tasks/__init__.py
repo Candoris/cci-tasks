2f65da0cdae2ae348adce12fddf2cd3093d93b3e
atrancandoris <atran@candoris.com>
2020-05-18 08:28:17 -0400

    major fixes

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,41 +2,81 @@ import json
 
 from cumulusci.salesforce_api.metadata import ApiRetrieveInstalledPackages
 from cumulusci.tasks.salesforce import BaseSalesforceTask
-from cumulusci.tasks.salesforce import BaseSalesforceMetadataApiTask
+from cumulusci.tasks.salesforce import BaseSalesforceApiTask
+from cumulusci.tasks.salesforce import UpdateDependencies
 
-class ResolveDependencies(BaseSalesforceMetadataApiTask):
-    api_class = ApiRetrieveInstalledPackages
-    name = "GetInstalledPackages"
 
-    def _add_dependency(self, packageDirectory):
-        packageDirectory["dependencies"] = self.dependencies
-        return packageDirectory
-
-    def _package_to_dependency(self, item):
-        return { 'package': f'{item[0]}@{item[1]}' }
+class ListDependencies(UpdateDependencies):
+    """
+    This class overrides a few key methods of the standard UpdateDependencies to resolve dependencies in manner defined by CumulusCI, while not actually installing the dependencies. Results in a List of dependencies.
+    """
 
     def _run_task(self):
-        result = super()._run_task()
+        self.results = []
+        super()._run_task()
+        return self.results
+
+    def _process_dependencies(self, dependencies):
+        for dependency in dependencies:
+            subdependencies = dependency.get("dependencies")
+            if subdependencies:
+                self._process_dependencies(subdependencies)
+
+            if ("namespace" in dependency):
+                self.results.append(dependency)
+
+    def _uninstall_dependencies(self):
+        None
+
+    def _uninstall_dependencies(self):
+        None
+
+    def _install_dependencies(self):
+        None
+
+
+class ResolveDependencies(ListDependencies, BaseSalesforceApiTask):
+    """
+    Adds package aliases to sfdx-project.json for each packageDirectory dependencies list. Also adds subscriber package version ids to list of packageAliases.
+    """
+
+    def _add_dependency(self, packageDirectory):
+        """
+        Helper function used in map() to add packages to each packageDirectories in sfdx-project.json
+        """
+        packageDirectory["dependencies"] = self.packages
+        return packageDirectory
+
+    def _run_task(self):
+        self.dependencies = []
         with open("sfdx-project.json", "r") as f:
             data = json.load(f)
 
-        # result = {
-        #     "npe01": "3.14",
-        #     "npe4": "3.10",
-        #     "npe03": "3.19",
-        #     "npo02": "3.12",
-        #     "npsp": "3.176",
-        #     "npe5": "3.8"
-        # }
-        #
-        # Converts above to managed package dependency format. See https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev2gp_config_file.htm
-        self.dependencies = list(map(self._package_to_dependency, result.items()))
+        # super() refers to ListDependencies
+        self.dependencies = super()._run_task()
+
+        # Get installed packages
+        self.installed_packages = self.tooling.query(
+            'SELECT SubscriberPackage.NamespacePrefix, SubscriberPackageVersionId FROM InstalledSubscriberPackage')
+
+        self.packages = []
+        for dependency in self.dependencies:
+            self.packages.append({'package': dependency["namespace"]})
+            # Add subscriber package version ids to packageAliases. For each namespace (e.g., packageAliases.npe01), lookup the installed package record and grab the SubscriberPackageVersionId.
+            data["packageAliases"][dependency["namespace"]] = next(
+                filter(
+                    lambda record: record["SubscriberPackage"]["NamespacePrefix"] == dependency[
+                        "namespace"], self.installed_packages["records"]
+                ), None
+            )["SubscriberPackageVersionId"]
 
         # For each package directory, add dependencies.
-        data["packageDirectories"] = list(map(self._add_dependency, data["packageDirectories"]))
+        data["packageDirectories"] = list(
+            map(self._add_dependency, data["packageDirectories"]))
 
         with open("sfdx-project.json", "w") as f:
             f.write(json.dumps(data, indent=2))
+
 
 class SetDefault(BaseSalesforceTask):
     task_options = {
@@ -56,7 +96,8 @@ class SetDefault(BaseSalesforceTask):
         with open("sfdx-project.json", "r") as f:
             data = json.load(f)
 
-        data["packageDirectories"] = list(map(self._set_default, data["packageDirectories"]))
+        data["packageDirectories"] = list(
+            map(self._set_default, data["packageDirectories"]))
 
         with open("sfdx-project.json", "w") as f:
             f.write(json.dumps(data, indent=2))
