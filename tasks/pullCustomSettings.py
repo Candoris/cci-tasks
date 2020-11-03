0a3669595f0751df4bfb88437c2d279d5460e760
kdmace11 <51129008+kdmace11@users.noreply.github.com>
2020-11-03 07:43:52 -0500

    Revised pullCustomSettings for lists/hierarchies (#11)
    Co-authored-by: Kurt Mace <kdmace@Kurts-MBP.lan>

--- tasks/pullCustomSettings.py
+++ tasks/pullCustomSettings.py
@@ -6,27 +6,33 @@ class PullSettings(BaseSalesforceApiTask):
 
     def _run_task(self):
         self.logger.info("Beginning to pull custom settings...")
-        unwantedFields = ["CreatedById", "CreatedDate", "Id", "IsDeleted", "LastModifiedById", "LastModifiedDate", "Name", "SetupOwnerId", "SystemModstamp"]
+        unwantedFields = ["CreatedById", "CreatedDate", "Id", "IsDeleted",
+                          "LastModifiedById", "LastModifiedDate", "SetupOwnerId", "SystemModstamp"]
 
         # Gather all settings from Salesforce
         res = self.sf.describe()
 
         masterList = {}
-        
+
         # Filter the entries to result in only custom settings
         customSettings = []
         for x in res["sobjects"]:
             if x["customSetting"]:
                 customSettings.append(x)
 
-        # for every custom setting
+        # For every custom setting
         for setting in customSettings:
             settingName = setting["name"]
             fields = getattr(self.sf, settingName).describe()["fields"]
+
+            isHierarchy = False
+
             # build the query for this setting
             first = True
             query = "SELECT"
             for field in fields:
+                if field["name"] == "Name":
+                    isHierarchy = field["nillable"]
                 if first:
                     query += " " + field["name"]
                     first = False
@@ -37,24 +43,42 @@ class PullSettings(BaseSalesforceApiTask):
             # execute the query
             queryResult = self.sf.query(query)
 
-            # if query is non-empty, add fields/values to masterList
+            # If query is non-empty, add fields/values to masterList using the appropriate format
             if queryResult["totalSize"] > 0:
-                settingProperties = { "location": "org" }
-                settingFields = {}
-                for field in fields:
-                    if field["name"] not in unwantedFields:
-                        name = field["name"]
-                        value = queryResult["records"][0][name]
+                if isHierarchy:
+                    # HIERARCHY
+                    settingProperties = {"location": "org"}
+                    settingFields = {}
+                    for field in fields:
+                        if field["name"] not in unwantedFields:
+                            name = field["name"]
+                            value = queryResult["records"][0][name]
 
-                        settingFields[name] = value
-                
-                settingProperties["data"] = settingFields
-                masterList[settingName] = [ settingProperties ]
+                            settingFields[name] = value
+
+                    settingProperties["data"] = settingFields
+                    masterList[settingName] = [settingProperties]
+                else:
+                    # LIST
+                    settingRecords = {}
+                    for i in range(queryResult["totalSize"]):
+                        recordDict = {}
+                        recordName = queryResult["records"][i]["Name"]
+                        for field in fields:
+                            if field["name"] not in unwantedFields and field["name"] != "Name":
+                                name = field["name"]
+                                value = queryResult["records"][i][name]
+
+                                recordDict[name] = value
+
+                        settingRecords[recordName] = recordDict
+
+                    masterList[settingName] = settingRecords
 
         self.logger.info("MasterList:")
         self.logger.info(masterList)
-            
-        # write the results to a yaml file
+
+        # Write the results to a yaml file
         yamlContents = yaml.safe_dump(masterList)
         f = open("orgCustomSettings.yml", "w")
         f.write(yamlContents)
